97220d75c9d3c5df442011292bbb6b06dee9def1
Amitay Molko <amitaymolko@gmail.com>
2018-05-11 04:13:38 +0300

    hashbrownies in da table

--- frontend/main.js
+++ frontend/main.js
@@ -1,9 +1,4 @@
-fetch("HashCracker.json").then((e) => e.json().then(function(text){
-  window.HashCrackerJson = text
-  window.HashCrackerAbi = web3.eth.contract(window.HashCrackerJson.abi);
-  window.HashCracker = window.HashCrackerAbi.at(window.HashCrackerJson['networks']['3'].address);
-  loadedHashCracker()
-}));
+
 
 function closeModal(tx) {
     $('#exampleModal').modal('hide');
@@ -12,37 +7,49 @@ function closeModal(tx) {
     document.getElementById('tx').innerText = tx;
 }
 
-function loadedHashCracker() {
-  loadHashBounties()
-}
+var app = angular.module("myApp", []);
+app.controller('hashBountiesCtrl', function ($scope, $http) {
+  fetch("HashCracker.json").then((e) => e.json().then(function (text) {
+    window.HashCrackerJson = text
+    window.HashCrackerAbi = web3.eth.contract(window.HashCrackerJson.abi);
+    window.HashCracker = window.HashCrackerAbi.at(window.HashCrackerJson['networks']['3'].address);
+    loadedHashCracker($scope)
+  }));
 
-function loadHashBounties() {
-  window.HashCracker.getHashCracksLength(function (err, value) {
-    if (err) {
-      console.log('err', err);
-    } else {
-      var length = value.toNumber();
-      window.hashBounties = [];
-      for (var index = 0; index < length; index++) {
-        loadHashBounty(index)
+  function loadedHashCracker($scope) {
+    loadHashBounties($scope)
+  }
+
+  function loadHashBounties($scope) {
+    window.HashCracker.getHashCracksLength(function (err, value) {
+      if (err) {
+        console.log('err', err);
+      } else {
+        var length = value.toNumber();
+        window.hashBounties = [];
+        for (var index = 0; index < length; index++) {
+          loadHashBounty(index, $scope)
+        }
       }
-    }
-  })
-}
+    })
+  }
 
-function loadHashBounty(index) {
-  window.HashCracker.hashCracks(index, function (err, value) {
-    if (err) {
-      console.log('err', err);
-    } else {
-      window.hashBounties[index] = value
-      reloadHashCrackes()
-    }
-  })
-}
 
-function reloadHashCrackes() {
-}
+  function loadHashBounty(index, $scope) {
+    window.HashCracker.hashCracks(index, function (err, value) {
+      if (err) {
+        console.log('err', err);
+      } else {
+        window.hashBounties[index] = value
+        $scope.$apply(function () {
+          $scope.hashBounties = window.hashBounties
+          console.log('$scope.hashBounties', $scope.hashBounties)
+        });
+      }
+    })
+  }
+});
+
 
 function pay() {
   console.log(document.forms['submit_hash'].hash_to_break.value, document.forms['submit_hash'].hash_type.value);
