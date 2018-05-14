bbd72320021f0436b955af8478121b966dafe431
Amitay Molko <amitaymolko@gmail.com>
2018-05-14 22:45:41 +0300

    addded scrypt bytes parsing

---
+++ contracts/Scrypt.sol
@@ -0,0 +1,115 @@
+pragma solidity 0.4.23;
+
+contract Scrypt {
+    event Log(bytes32 hash);
+
+    struct ScryptHashParam {
+        bytes32 hash;
+
+        bytes32 ciphertext;
+        uint8 dklen;
+        bytes32 salt;
+        uint16 n;
+        uint8 r;
+        uint8 p;
+        bytes32 mac;
+    }
+
+    mapping (bytes32 => ScryptHashParam) public scryptHashParams;
+
+    modifier hashMatches(bytes32 _hash, bytes _input) {
+        require(_hash == keccak256(_input));
+        _;
+    }
+
+    modifier validBytesLength(bytes _input) {
+        require(_input.length == 101);
+        _;
+    }
+
+    function parseScryptInput(bytes _input)
+    private 
+    pure
+    validBytesLength(_input)
+    returns(
+        bytes32 ciphertext,
+        uint8 dklen,
+        bytes32 salt,
+        uint16 n,
+        uint8 r,
+        uint8 p,
+        bytes32 mac
+    ) {
+        
+        ciphertext = bytesToBytes32(_input, 0);
+        dklen = bytesToUint8(_input, 32);
+        salt = bytesToBytes32(_input, 33);
+        n = bytesToUint16(_input, 65);
+        r = bytesToUint8(_input, 67);
+        p = bytesToUint8(_input, 68);
+        mac = bytesToBytes32(_input, 69);
+    }
+
+    function bytesToBytes32(bytes b, uint offset) private pure returns (bytes32) {
+        bytes32 out;
+
+        for (uint i = 0; i < 32; i++) {
+            out |= bytes32(b[offset + i] & 0xFF) >> (i * 8);
+        }
+        return out;
+    }
+
+    function bytesToUint8(bytes b, uint offset) private pure returns (uint8) {
+        uint8 out;
+        out |= uint8(b[offset] & 0xFF) >> (8);
+        return out;
+    }
+
+    function bytesToUint16(bytes b, uint offset) private pure returns (uint16) {
+        bytes2 out;
+
+        for (uint i = 0; i < 2; i++) {
+            out |= bytes2(b[offset + i] & 0xFF) >> (i * 8);
+        }
+        return uint16(out);
+    }
+
+    function requestScryptHashCrack(bytes32 _hash, bytes _input)
+    hashMatches(_hash, _input)
+    // payable
+    public 
+    {
+        bytes32 ciphertext;
+        uint8 dklen;
+        bytes32 salt;
+        uint16 n;
+        uint8 r;
+        uint8 p;
+        bytes32 mac;
+
+        (ciphertext, dklen, salt, n, r, p, mac) = parseScryptInput(_input);
+        scryptHashParams[_hash] = ScryptHashParam(_hash, ciphertext, dklen, salt, n, r, p, mac);
+    }
+
+    function getScrypt(bytes32 _hash)
+    public 
+    view
+    returns (
+        bytes32 ciphertext,
+        uint8 dklen,
+        bytes32 salt,
+        uint16 n,
+        uint8 r,
+        uint8 p,
+        bytes32 mac
+    ) {
+        ScryptHashParam storage scryptHashParam = scryptHashParams[_hash];
+        ciphertext = scryptHashParam.ciphertext;
+        dklen = scryptHashParam.dklen;
+        salt = scryptHashParam.salt;
+        n = scryptHashParam.n;
+        r = scryptHashParam.r;
+        p = scryptHashParam.p;
+        mac = scryptHashParam.mac;
+    }
+}
---
+++ test/Scrypt.js
@@ -0,0 +1,38 @@
+const Scrypt = artifacts.require('Scrypt')
+
+contract('Scrypt', (accounts) => {
+  
+  var account = accounts[0]
+  var account2 = accounts[1]
+
+  const minValue = web3.toWei(0.001, 'ether')
+  const inputBytes = "0x19af5e7a09875927cdec8900a5b4ce1e428b6746836586ea6838d1cf5a0271152052d7e775c9b3a5d8ebf40e2be3be234707816a758b6eec1ba28c54a4b6cf63de200008010994fb7a49c0f5f4b2c365e168f27701366acb3c326c11366b027eb96119140b"
+
+  it('deploy and grab', async () => {
+    Contract = await Scrypt.new()
+  }) 
+
+  // it('fails to submit hashcrack with invalid amoun', async () => {
+  //   try {
+  //     const value = web3.toWei(0.0001, 'ether')
+  //     const tx = await Contract.requestHashCrack(0x19513FDC9DA4FB72A4A05EB66917548D3C90FF94D5419E1F2363EEA89DFEE1DD, "sha256", { value })
+  //     console.log('tx', tx)
+  //     throw new Error('unauthorized tx')
+  //   } catch (err) {
+  //     if (!err.message.endsWith('revert')) {
+  //       throw err
+  //     }
+  //     return
+  //   }
+  // })
+
+  it('fires NewHashEvent', async() => {
+    const hash = web3.sha3(inputBytes, { encoding: "hex" })
+    await Contract.requestScryptHashCrack(hash, inputBytes) 
+    const scryptHashParam = await Contract.getScrypt(hash)
+    console.log('scryptHashParam', scryptHashParam[3].toString())
+
+    // assert.isTrue(false)
+  })
+
+})
